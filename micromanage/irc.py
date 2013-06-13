cd941452f7065c9de64a833b1cd22c2e550f037f
Shiz <mark@xn--hwg34fba.ws>
2013-06-13 17:09:17 +0200

    Added proper nickname registration checking.

--- micromanage/irc.py
+++ micromanage/irc.py
@@ -3,6 +3,7 @@
 
 import os
 import threading
+import time
 
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol, ssl
@@ -67,6 +68,10 @@ def add_handler(command, handler):
     handlers[command].append(handler)
 
 class Bot(irc.IRCClient):
+    def __init__(self):
+        super(irc.IRCClient, self).__init__()
+        self.nick_status = {}
+
     def signedOn(self):
         # Identify.
         if config.irc_pass is not None:
@@ -75,17 +80,30 @@ class Bot(irc.IRCClient):
         # Auto-join channels.
         for channel in config.irc_channels:
             self.join(channel)
+    
+    def userLeft(self, user, channel):
+        # Clear nick registration status.
+        user = user.split('!', 1)[0]
+        if user in self.nick_status:
+            self.nick_status.remove(user)
 
-    def is_admin(self, user):
-        self.whois(user)
-        # TODO: Implement.
-        return True
+    def userQuit(self, user, message):
+        # Clear nick registration status.
+        user = user.split('!', 1)[0]
+        if user in self.nick_status:
+            self.nick_status.remove(user)
 
-    def respond(self, user, channel, message):
-        if channel == self.nickname:
-            self.msg(user, message)
-        else:
-            self.msg(channel, message)
+    def userKicked(self, user, channel, kicker, message):
+        # Clear nick registration status.
+        user = user.split('!', 1)[0]
+        if user in self.nick_status:
+            self.nick_status.remove(user)
+
+    def userRenamed(self, user, new):
+        # Clear nick registration status.
+        user = user.split('!', 1)[0]
+        if user in self.nick_status:
+            self.nick_status.remove(user)
 
     def privmsg(self, user, channel, message):
         global handlers
@@ -99,6 +117,38 @@ class Bot(irc.IRCClient):
                         handler(self, user, channel, message)
                     except Exception as e:
                         self.respond(user, channel, '{b}Error while executing {cmd}:{b} {type} - {error}'.format(type=e.__class__.__name__, error=e, cmd=handler.__name__, **commands))
+
+    def irc_unknown(self, server, code, params):
+        # Check nickname registration status.
+        if code == '307':
+            message = params[0]
+            target, user, contents = message
+
+            if 'identified' in contents:
+                self.nick_status[user] = True
+        # If we reached the end of a whois status with no registered line, it's not registered.
+        elif code == 'RPL_ENDOFWHOIS':
+            message = params[0]
+            target, user, contents = message
+            if user not in self.nick_status:
+                self.nick_status[user] = False
+
+
+    def is_admin(self, user):
+        if user not in self.nick_status:
+            self.whois(user)
+
+        # Wait until we have fetched registration status.
+        while user not in self.nick_status:
+            time.sleep(0.1)
+
+        return self.nick_status[user]
+
+    def respond(self, user, channel, message):
+        if channel == self.nickname:
+            self.msg(user, message)
+        else:
+            self.msg(channel, message)
 
 class BotFactory(protocol.ClientFactory):
     def buildProtocol(self, addr):
